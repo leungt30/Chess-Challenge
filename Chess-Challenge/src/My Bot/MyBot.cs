f213a8091546236298c34485ab73ed0b0d6b0eea
Timothy Leung <leungt30@mcmaster.ca>
2024-06-11 22:28:11 -0400

    add some basic structure to mybot

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -5,6 +5,45 @@ public class MyBot : IChessBot
     public Move Think(Board board, Timer timer)
     {
         Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        
+        int scoreMult = board.IsWhiteToMove ? 1 : -1 ;
+        decimal bestEval = decimal.MinValue;
+        Move bestMove = moves[0];
+        foreach (Move move in moves)
+        {
+            // Always play checkmate in one
+            if (MoveIsCheckmate(board, move))
+            {
+                return move;
+            }
+            board.MakeMove(move);
+            decimal eval = Evaluate(board) * scoreMult;
+
+            if (eval > bestEval)
+            {
+                bestEval = eval;
+                bestMove = move;
+            }
+
+
+            board.UndoMove(move);
+    
+        }
+        return bestMove;
     }
+
+    public decimal Evaluate(Board board)
+    {
+        // Call python script to evaluate the board
+        return 0;
+    }
+
+
+    private bool MoveIsCheckmate(Board board, Move move)
+        {
+            board.MakeMove(move);
+            bool isMate = board.IsInCheckmate();
+            board.UndoMove(move);
+            return isMate;
+        }
 }
